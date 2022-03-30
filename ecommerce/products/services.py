0ce088fd182bd3eb4eb3db33e77d4c2195ed83ad
Juan Pablo Senn <juanpsenn@gmail.com>
2022-03-30 19:02:46 -0300

    Added not enought stock validation

--- ecommerce/products/services.py
+++ ecommerce/products/services.py
@@ -1,9 +1,16 @@
+from django.core.exceptions import ValidationError
+
 from . import models
 
 
 def stock_update(product: models.Product, quantity: int):
     """
-    Update the stock of a product.
+    Update the stock of a product if has enought stock.
+    Otherwise, raise ValidationError.
     """
     product.stock += quantity
+
+    if product.stock < 0:
+        raise ValidationError(f"Product {product.name} has no stock left.")
+
     product.save()
